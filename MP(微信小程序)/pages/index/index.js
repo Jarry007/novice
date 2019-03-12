f394afa4c570b7df6daaaecc8ffda0e2c6e50e20
Jarry007 <940615834@qq.com>
2019-03-12 20:43:05 +0800

    css3加载动画

--- MP(微信小程序)/pages/index/index.js
+++ MP(微信小程序)/pages/index/index.js
@@ -11,11 +11,7 @@ Page({
     src:'',
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
+
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
